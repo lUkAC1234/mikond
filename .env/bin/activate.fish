b3795205b174dacc372afe1b31d55dbfe62d048b
root <root@vmi1321452.contaboserver.net>
2023-06-06 07:41:14 +0200

    mikond-updated-

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/fdp-3-22/django/mikond/.env"
+set -gx VIRTUAL_ENV "/root/var/www/mikond/.env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

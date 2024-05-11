e5b82bc36211b8727843c3d12b37cd76dd6222bb
lUkAC2 <113376248+lUkAC1234@users.noreply.github.com>
2024-05-11 22:51:56 +0500

    New Beginning era

--- web/urls.py
+++ web/urls.py
@@ -1,17 +1,5 @@
 from django.urls import path
-from .views import main, about, projects, collections, contactus, detailprojects, \
-    detailcollections, contactdetail, signin, signup, logoutView, adminPanel
+from .views import main
 urlpatterns = [
     path('', main.as_view(), name="main"),
-    path('adminpanel/', adminPanel, name="adminpanel"),
-    path('about/', about.as_view(), name="about"),
-    path('projects/', projects.as_view(), name="projects"),
-    path('collections/', collections.as_view(), name="collections"),
-    path('contact/', contactus.as_view(), name="contact"),
-    path('contactdetail/<int:pk>', contactdetail.as_view(), name="contactdetail"),
-    path('detail-projects/<int:pk>', detailprojects.as_view(), name="detail-projects"),
-    path('detail-collections/<int:pk>', detailcollections.as_view(), name="detail-collections"),
-    path('signup/', signup, name='signup'),
-    path('signin/', signin, name='signin'),
-    path('logout/', logoutView, name='logout'),
 ]

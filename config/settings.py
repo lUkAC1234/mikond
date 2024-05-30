34da86854a929336fb43ea797884cd5bb9c882da
lUkAC2 <113376248+lUkAC1234@users.noreply.github.com>
2024-05-30 23:05:45 +0500

    little changes

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mikond',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'dimatop12',
         'HOST': 'localhost',
         'PORT': '5432'
